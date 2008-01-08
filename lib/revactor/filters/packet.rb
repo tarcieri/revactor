ad5ba94f792a6875f324dee793ff7365145329d6
Tony Arcieri <tony@medioh.com>
2008-01-08 07:39:23 +0000

    Packet filter and spec

---
+++ lib/revactor/filters/packet.rb
@@ -0,0 +1,88 @@
+#--
+# Copyright (C)2007 Tony Arcieri
+# You can redistribute this under the terms of the Ruby license
+# See file LICENSE for details
+#++
+
+module Revactor
+  module Filters
+    class Packet
+      # Class for processing size prefixes in packet frames
+      class Prefix
+        attr_reader :size, :data
+
+        def initialize(size = 2)
+          unless size == 2 or size == 4
+            raise ArgumentError, 'only 2 or 4 byte prefixes are supported' 
+          end
+
+          @size = size
+          reset!
+        end
+
+        # Has the entire prefix been read yet?
+        def read?
+          @size == @data.size
+        end
+
+        # Append data to the prefix and return any extra
+        def append(data)
+          @data << data.slice!(0, @size - @data.size)
+          return unless read?
+
+          @payload_length = @data.unpack(@size == 2 ? 'n' : 'N').first
+          data
+        end
+
+        # Length of the payload extracted from the prefix
+        def payload_length
+          raise RuntimeError, 'payload_length called before prefix extracted' unless read?
+          @payload_length
+        end
+
+        def reset!
+          @payload_length = nil
+          @data = ''        
+        end
+      end
+
+      def initialize(size = 2)
+        @prefix = Prefix.new(size)
+        @buffer = ''
+      end
+
+      # Callback for processing incoming frames
+      def decode(data)
+        received = []
+        
+        begin
+          # Read data and append it to the size prefix unless it's already been read
+          data = @prefix.append(data) unless @prefix.read?
+          return received if data.nil? or data.empty?
+
+          # If we've read the prefix, append the data
+          @buffer << data
+
+          # Don't do anything until we receive the specified amount of data
+          return unless @buffer.size >= @prefix.payload_length
+
+          # Extract the specified amount of data and process it
+          received << @buffer.slice!(0, @prefix.payload_length)
+
+          # Reset the prefix and buffer since we've received a whole frame
+          @prefix.reset!
+          data = @buffer
+          @buffer = ''
+        end until data.nil? or data.empty?
+        
+        received
+      end
+
+      # Send a packet with a specified size prefix
+      def encode(data)
+        raise ArgumentError, 'packet too long for prefix length' if data.size >= 256**@prefix.size
+        [data.size].pack(@prefix.size == 2 ? 'n' : 'N') << data
+      end
+    end
+  end
+end
---
+++ spec/packet_filter_spec.rb
@@ -0,0 +1,111 @@
+#--
+# Copyright (C)2007 Tony Arcieri
+# You can redistribute this under the terms of the Ruby license
+# See file LICENSE for details
+#++
+
+require File.dirname(__FILE__) + '/../lib/revactor/filters/packet.rb'
+
+describe Revactor::Filters::Packet do
+  describe 'Prefix' do
+    it "supports 2 or 4-byte prefixes" do
+      proc { [2,4].each { |n| Revactor::Filters::Packet::Prefix.new(n) } }.should_not raise_error
+    end
+  
+    it "raises an exception if instantiated with an invalid prefix size" do
+      proc { Revactor::Filters::Packet::Prefix.new(3) }.should raise_error
+    end
+  
+    it "uses a 2-byte prefix as the default" do
+      Revactor::Filters::Packet::Prefix.new.size.should == 2
+    end
+  
+    it "decodes 2-byte prefixes from network byte order" do
+      @prefix = Revactor::Filters::Packet::Prefix.new(2)
+      @prefix.append [42].pack('n')
+      @prefix.payload_length.should == 42
+    end
+  
+    it "decodes 4-byte prefixes from network byte order" do
+      @prefix = Revactor::Filters::Packet::Prefix.new(4)
+      @prefix.append [42].pack('N')
+      @prefix.payload_length.should == 42
+    end
+  
+    it "allows the prefix to be written incrementally" do
+      @prefix = Revactor::Filters::Packet::Prefix.new(2)
+      value = [42].pack('n')
+    
+      @prefix.append value[0..0]
+      @prefix.read?.should be_false
+      proc { @prefix.payload_length }.should raise_error
+    
+      @prefix.append value[1..1]
+      @prefix.read?.should be_true
+      @prefix.payload_length.should == 42
+    end
+  
+    it "returns data which exceeds the prefix length" do
+      @prefix = Revactor::Filters::Packet::Prefix.new(2)
+      @prefix.append([42].pack('n') << "extra").should == 'extra'
+    end
+  
+    it "resets to a consistent state after being used" do
+      @prefix = Revactor::Filters::Packet::Prefix.new(2)
+      @prefix.append [17].pack('n')
+    
+      @prefix.reset!
+      @prefix.read?.should be_false
+      proc { @prefix.payload_length }.should raise_error
+    
+      @prefix.append [21].pack('n')
+      @prefix.payload_length.should == 21
+    end
+  end
+
+  before(:each) do
+    @payload = "A test string"
+  end
+  
+  it "decodes frames with 2-byte prefixes" do
+    filter = Revactor::Filters::Packet.new(2)
+    filter.decode([@payload.length].pack('n') << @payload).should == [@payload]
+  end
+  
+  it "encodes frames with 2-byte prefixes" do
+    filter = Revactor::Filters::Packet.new(2)
+    filter.encode(@payload).should == [@payload.length].pack('n') << @payload
+  end
+    
+  it "decodes frames with 4-byte prefixes" do
+    @filter = Revactor::Filters::Packet.new(4)    
+    @filter.decode([@payload.length].pack('N') << @payload).should == [@payload]
+  end
+  
+  it "encodes frames with 4-byte prefixes" do
+    @filter = Revactor::Filters::Packet.new(4)    
+    @filter.encode(@payload).should == [@payload.length].pack('N') << @payload
+  end
+  
+  it "reassembles fragmented frames" do
+    filter = Revactor::Filters::Packet.new(2)
+    
+    msg1 = 'foobar'
+    msg2 = 'baz'
+    
+    packet1 = [msg1.length].pack('n') << msg1
+    packet2 = [msg2.length].pack('n') << msg2
+        
+    chunks = []
+    chunks[0] = packet1[0..0]
+    chunks[1] = packet1[1..(packet1.size - 1)] << packet2[0..0]
+    chunks[2] = packet2[1..(packet2.size - 1)]
+    
+    chunks.reduce([]) { |a, chunk| a + filter.decode(chunk) }.should == [msg1, msg2]
+  end
+
+  it "raises an exception for overlength frames" do
+    payload = 'X' * 65537
+    proc { @filter.encode payload }.should raise_error
+  end
+end
