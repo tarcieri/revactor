9ff3984fb66756986ba17378d68ed1373cae9a45
Tony Arcieri <tony@medioh.com>
2008-01-18 04:05:12 +0000

    Document the echo server

--- examples/echo_server.rb
+++ examples/echo_server.rb
@@ -1,18 +1,33 @@
+# An example echo server, written using Revactor::TCP
+# This implementation creates a new actor for each
+# incoming connection.
+
 require File.dirname(__FILE__) + '/../lib/revactor'
 
 HOST = 'localhost'
 PORT = 4321
 
+# Before we can begin using actors we have to call Actor.start
+# Future versions of Revactor will hopefully eliminate this
 Actor.start do
+  # Create a new listener socket on the given host and port
   listener = Revactor::TCP.listen(HOST, PORT)
   puts "Listening on #{HOST}:#{PORT}"
 
+  # Begin receiving connections
   loop do
+    # Accept an incoming connection and start a new Actor
+    # to handle it
     Actor.spawn(listener.accept) do |sock|
+
+      # Begin echoing received data
       loop do
         begin
+          # Write everything we read
           sock.write sock.read
         rescue EOFError
+          # Break (and exit the current actor) if the connection
+          # is closed, just like with a normal Ruby socket
           break
         end
       end
