0b2233f62f3bd29417820c792772e4e42045e328
Tony Arcieri <tony@medioh.com>
2008-02-23 22:49:12 +0000

    Spruce up chat server and rename tools to benchmarks

--- examples/chat_server.rb
+++ examples/chat_server.rb
@@ -1,17 +1,35 @@
+#
+# A simple chat server implemented using 1 <-> N actors 
+# 1 server, N client managers, plus a listener
+#
+# The server handles all message formatting, traffic routing, and connection tracking
+# Client managers handle connection handshaking as well as low-level network interaction
+# The listener spawns new client managers for each incoming connection
+#
+
 require File.dirname(__FILE__) + '/../lib/revactor'
 
 HOST = 'localhost'
 PORT = 4321
 
+# Open a listen socket.  All traffic on new connections will be run through
+# the "line" filter, so incoming messages are delimited by newlines.
+
 listener = Actor::TCP.listen(HOST, PORT, :filter => :line)
 puts "Listening on #{HOST}:#{PORT}"
 
+# Spawn the server
 server = Actor.spawn do
   clients = {}
+  
+  # A proc to broadcast a message to all connected clients.  If the server
+  # were encapsulated into an object this could be a method
   broadcast = proc do |msg|
     clients.keys.each { |client| client << T[:write, msg] }
   end
   
+  # Server's main loop.  The server handles incoming messages from the
+  # client managers and dispatches them to other client managers.
   loop do 
     Actor.receive do |filter|
       filter.when(T[:register]) do |_, client, nickname|
@@ -33,18 +51,26 @@ server = Actor.spawn do
   end
 end
 
+# The main loop handles incoming connections
 loop do
+  # Spawn a new actor for each incoming connection
   Actor.spawn(listener.accept) do |sock|
     puts "#{sock.remote_addr}:#{sock.remote_port} connected"
 
+    # Connection handshaking
     begin
       sock.write "Please enter a nickname:"
       nickname = sock.read
 
       server << T[:register, Actor.current, nickname]
+      
+      # Flip the socket into asynchronous "active" mode
+      # This means the Actor can receive messages from
+      # the socket alongside other events.
       sock.controller = Actor.current
       sock.active = :once
     
+      # Main message loop
       loop do
         Actor.receive do |filter|
           filter.when(T[:tcp, sock]) do |_, _, message|
